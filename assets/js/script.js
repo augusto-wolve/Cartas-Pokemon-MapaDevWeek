6afa793c22f4ef7e7945c7a2e550904996afe6fc
Augusto Cesar <augustoc1130@hotmail.com>
2022-08-08 20:10:22 -0300

    implementação de javascript

---
+++ assets/js/script.js
@@ -0,0 +1,47 @@
+const btnAvancar = document.getElementById('btn-avancar');
+const btnVoltar = document.getElementById('btn-voltar');
+const cartoes = document.querySelectorAll('.cartao');
+var cartaoAtual = 0
+
+function esconderCartaoSelecionado() {
+
+    const cartaoSelecionado = document.querySelector('.cartao-selecionado');
+
+    cartaoSelecionado.classList.remove('cartao-selecionado');
+
+}
+
+
+btnAvancar.addEventListener('click', function () {
+
+
+    if (cartaoAtual === cartoes.length - 1) {
+        return
+    }
+    esconderCartaoSelecionado();
+
+    cartaoAtual++;
+    cartoes[cartaoAtual].classList.add('cartao-selecionado');
+
+
+
+
+
+})
+
+btnVoltar.addEventListener('click', function () {
+
+    if (cartaoAtual === 0) {
+        return
+    }
+
+
+    esconderCartaoSelecionado();
+
+    cartaoAtual--;
+    cartoes[cartaoAtual].classList.add('cartao-selecionado');
+
+
+
+
+})
